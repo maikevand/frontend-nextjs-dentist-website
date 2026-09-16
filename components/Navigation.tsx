fccf12f42b863ec35c0c8fb2e2904b4eb09de614
maike1983 <Maikevandartel@outlook.com>
2026-09-16 17:13:01 +0200

    Opzet project incl navbar

---
+++ components/Navigation.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Navigation() {
+    return (
+        <nav>
+            <div className="navigation-container">
+                <div className="navigation-company-name">
+                    De Tandenborstel
+                </div>
+                <div className="navigation-">
+                    <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/gaatjes">Gaatjes</Link></li>
+            <li><Link href="/bleken">Bleken</Link></li>
+            <li><Link href="/afspraken" className="navigation-button">Afspraken</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
